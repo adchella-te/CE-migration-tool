e9a0229cdc80838ee34a88e405ad2d194584e24c
adchella-te <101803940+adchella-te@users.noreply.github.com>
2022-10-10 23:51:55 +0200

    Update eu-migration-tool.py
    Added full coverage for A2S tests

--- eu-migration-tool.py
+++ eu-migration-tool.py
@@ -23,9 +23,8 @@ def create_bgp_test():
     })
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'bgp' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
-    print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
+    if response.status_code != 201:
+        print("Error Code : "+ response.status_code + "\nError Message : " + response.text)        
 
 def create_rtp_test():
     global headers
@@ -40,9 +39,7 @@ def create_rtp_test():
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'voice' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
-
+    
 def create_sip_test():
     global headers
     payload = json.dumps({
@@ -60,8 +57,6 @@ def create_sip_test():
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'sip-server' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
 
 def create_ftp_test():
     global headers
@@ -80,8 +75,6 @@ def create_ftp_test():
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'ftp-server' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)  
 
 def create_transaction_test():
     global headers
@@ -97,8 +90,6 @@ def create_transaction_test():
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'web-transactions' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
 
 def create_page_test():
     global headers
@@ -114,8 +105,6 @@ def create_page_test():
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'page-load' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
 
 def create_http_test():
     global headers
@@ -130,8 +119,6 @@ def create_http_test():
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'http-server' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
 
 def create_dnssec_test():
     global headers
@@ -146,9 +133,7 @@ def create_dnssec_test():
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'dns-dnssec' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
-
+    
 def create_dnstrace_test():
     global headers
     payload = json.dumps({
@@ -162,8 +147,6 @@ def create_dnstrace_test():
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'dns-trace' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
     
 def create_dnsserv_test():
     global headers
@@ -179,8 +162,6 @@ def create_dnsserv_test():
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'dns-server' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
 
 def create_a2a_test():
     global headers
@@ -195,8 +176,6 @@ def create_a2a_test():
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'agent-to-agent' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
 
 def create_a2s_test():
     global headers
@@ -207,13 +186,30 @@ def create_a2s_test():
     "interval": int(fetch_test_details.tinterval),
     "agents": json.loads(fetch_test_details.tagents),
     "server": str(fetch_test_details.tserver.split(":")[0]),
-    "port": fetch_test_details.tport
+    "port": fetch_test_details.tport,
+    "protocol": str(fetch_test_details.tprotocol),
+    "probeMode": str(fetch_test_details.tprobeMode),
+    "pathTraceMode": str(fetch_test_details.tpathTraceMode),
+    "continuousMode": int(fetch_test_details.tcontinuousMode),
+    "networkMeasurements": int(fetch_test_details.tnetworkMeasurements),
+    "mtuMeasurements": str(fetch_test_details.tmtuMeasurements),
+    "bgpMeasurements": str(fetch_test_details.tbgpMeasurements),
+    "pingPayloadSize": int(fetch_test_details.tpingPayloadSize),
+    "numPathTraces":int(fetch_test_details.tnumPathTraces), 
+    "dscp": str(fetch_test_details.tdscp),
+    "dscpId": str(fetch_test_details.tdscpId),
+    "ipv6Policy": str(fetch_test_details.tipv6Policy)
     })
+    if fetch_test_details.tfixedPacketRate!= 0:
+        final_payload = payload[:-1]
+        final_payload = final_payload + ', ' + "\"fixedPacketRate\":" + str(fetch_test_details.tfixedPacketRate) + "}"
+    else:
+        final_payload = payload
+
     create_tests_url = "https://api.thousandeyes.com/v6/tests/" + 'agent-to-server' + "/new.json"
-    response = requests.request("POST", create_tests_url, headers=headers, data=payload)
+    response = requests.request("POST", create_tests_url, headers=headers, data=final_payload)
+    pprint(final_payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
     
 def fetch_test_details():
     payload = {}
@@ -240,8 +236,8 @@ def fetch_test_details():
                 fetch_test_details.tincludeCoveredPrefixes = tests_data['test'][i]['includeCoveredPrefixes']
                 fetch_test_details.tusePublicBgp = tests_data['test'][i]['usePublicBgp']
                 create_bgp_test()
-            except(e):
-                print("Error migrating BGP tests: " +  e)
+            except Exception as e:
+                print("Error migrating BGP tests: " +  str(e))
         
         if str(tests_data['test'][i]['type']) == 'voice':
             try:
@@ -448,10 +444,25 @@ def fetch_test_details():
                 else:
                     fetch_test_details.tport = None
                 #Optional
+                fetch_test_details.tprotocol = tests_data['test'][i]['protocol']    
+                fetch_test_details.tprobeMode = tests_data['test'][i]['probeMode']
+                fetch_test_details.tpathTraceMode = tests_data['test'][i]['pathTraceMode']    
+                fetch_test_details.tnetworkMeasurements = tests_data['test'][i]['networkMeasurements']
+                fetch_test_details.tcontinuousMode = tests_data['test'][i]['continuousMode']
+                fetch_test_details.tmtuMeasurements = tests_data['test'][i]['mtuMeasurements']
+                fetch_test_details.tbgpMeasurements = tests_data['test'][i]['bgpMeasurements']
+                fetch_test_details.tpingPayloadSize = tests_data['test'][i]['pingPayloadSize']
+                if('fixedPacketRate' in tests_data['test'][i]):
+                    fetch_test_details.tfixedPacketRate = tests_data['test'][i]['fixedPacketRate']
+                else:
+                    fetch_test_details.tfixedPacketRate = 0
+                fetch_test_details.tnumPathTraces = tests_data['test'][i]['numPathTraces']
+                fetch_test_details.tdscp = tests_data['test'][i]['dscp']
+                fetch_test_details.tdscpId = tests_data['test'][i]['dscpId']
+                fetch_test_details.tipv6Policy = tests_data['test'][i]['ipv6Policy']
                 create_a2s_test()
             except Exception as e:
-                print("Error migrating Agent to Agent tests: " +  str(e))
-
+                print("Error migrating Agent to Server tests: " +  str(e))
 
 
 def create_epa_a2s_test():
@@ -467,8 +478,6 @@ def create_epa_a2s_test():
     create_tests_url = "https://api.thousandeyes.com/v6/endpoint-tests/" + 'agent-to-server' + "/new.json"
     response = requests.request("POST", create_tests_url, headers=headers, data=payload)
     print(response.status_code)        
-    if response.status_code != 200 or response.status_code != 201:
-        print(response.text)
     
 def create_epa_http_test():
     global headers
